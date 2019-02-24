eb857f3dea941b3c3c95758df1f1829280264a5c
Charles Cauche <charles.cauche@gmail.com>
2019-02-24 20:11:25 +0100

    Feature 10 working

--- OrDic.py
+++ OrDic.py
@@ -1,90 +1,107 @@
 class OrDic():
 	def __init__(self, *dic, **args):
-		self.keys = []
-		self.values = []
+		self.keys_list = []
+		self.values_list = []
 		if len(dic)==1 and type(dic[0])==dict : #the *dic is a dictionary
 			dic = dic[0]
 			for i,j in dic.items():
-				self.keys.append(i)
-				self.values.append(j)
+				self.keys_list.append(i)
+				self.values_list.append(j)
 			print("New object created from a dictionary")
 		else : #the arg is not a dictionary
 			for i,j in args.items():
-				self.keys.append(i)
-				self.values.append(j)
+				self.keys_list.append(i)
+				self.values_list.append(j)
 			print("New object created from key/value")
 
 	def __repr__(self):
 		text_list = []
-		for i in range(len(self.keys)) :
-			text_list += ["{}: {}".format(self.keys[i],self.values[i])]
+		for i in range(len(self.keys_list)) :
+			text_list += ["{}: {}".format(self.keys_list[i],self.values_list[i])]
 		text = "{" + ", ".join(text_list) + "}"
 		return text
 		
 
 	def __getattr__(self, key) :
-		if key in self.keys:
-			index = self.keys.index(key)
-			return self.values[index]
+		if key in self.keys_list:
+			index = self.keys_list.index(key)
+			return self.values_list[index]
 		else :
 			return "sorry, this value does not exist in the OrDic"
 
 	# def __setattr__(self,key, value)
-	# 	if key in self.keys:
+	# 	if key in self.keys_list:
 	
 	def __getitem__(self,key) :
-		if key in self.keys:
-			index = self.keys.index(key)
-			return self.values[index]
+		if key in self.keys_list:
+			index = self.keys_list.index(key)
+			return self.values_list[index]
 		else :
 			return "sorry, this value does not exist in the OrDic"
 
 	def __setitem__(self,key,value) :
-		if key in self.keys: # if key already exist, then i update the existing value
-			index = self.keys.index(key)
-			self.values[index] = value
+		if key in self.keys_list: # if key already exist, then i update the existing value
+			index = self.keys_list.index(key)
+			self.values_list[index] = value
 			print("Value updated for key :", key)
 		else : # if key does not exist, then i add the value to the OrDic
-			self.keys.append(key)
-			self.values.append(value)
+			self.keys_list.append(key)
+			self.values_list.append(value)
 			print("New key/value created :", key, " / ",value)
 
 	def __delitem__(self, key):
-		if key in self.keys: 
-			index = self.keys.index(key)
-			del self.keys[index] 
-			del self.values[index]
+		if key in self.keys_list: 
+			index = self.keys_list.index(key)
+			del self.keys_list[index] 
+			del self.values_list[index]
 			print("Key/value deleted")
 		else : 
 			print("This key does not existe in this dictionnary")
 
 	def __contains__(self, key):
-		if key in self.keys :
+		if key in self.keys_list :
 			return True
 		else :
 			return False
 
 	def __len__(self):
-		return len(self.keys)
+		return len(self.keys_list)
 
 	def sort(self):
-		new_keys = sorted(self.keys)
+		new_keys = sorted(self.keys_list)
 		new_values = []
 		for i in new_keys:
 			new_values += [self[i]]
-		self.keys = new_keys
-		self.values = new_values
+		self.keys_list = new_keys
+		self.values_list = new_values
 
 	def reverse(self):
-		new_keys = sorted(self.keys, reverse=True)
+		new_keys = sorted(self.keys_list, reverse=True)
 		new_values = []
 		for i in new_keys:
 			new_values += [self[i]]
-		self.keys = new_keys
-		self.values = new_values
+		self.keys_list = new_keys
+		self.values_list = new_values
 
 	def keys(self):
-		return 1
+		for i in self.keys_list:
+			yield i
+
+	def values(self):
+		for i in self.values_list:
+			yield i
+
+	def items(self):
+		for i in self.keys_list:
+			yield i,self[i]
+
+
+
+
+
+	#def __iter__
+
+
 
 
 
@@ -146,3 +163,14 @@ c.sort()
 print(c)
 c.reverse()
 print(c)
+
+
+print("-----------------")
+print("Test 6")
+for i in c.keys():
+	print(i)
+for i in c.values():
+	print(i)
+c.sort()
+for i in c.items():
+	print(i)
